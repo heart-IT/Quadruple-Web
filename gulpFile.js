13dfc0deaca85d9eeadc443bf23b2512cc7c2300
Rahul Garg <rahul_garg08@yahoo.com>
2017-07-17 13:49:42 +0530

    added gulp andm inified quad

---
+++ gulpFile.js
@@ -0,0 +1,12 @@
+var gulp = require("gulp");
+var concat = require("gulp-concat");
+
+var uglify = require("gulp-uglify");
+
+gulp.task("scripts", function() {
+  gulp
+    .src(["./libs/idlie.js", "./libs/slider.js", "./quadRuple.js"])
+    .pipe(concat("quadRuple.min.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest("./"));
+});
